254a4dca760f465fdc3d94726b4f05b165514885
root <root@localhost.localdomain>
2020-10-04 16:32:13 -0400

    ARP reqiust comm

--- n_scanner.py
+++ n_scanner.py
@@ -3,10 +3,14 @@
 import scapy.all as scap
 
 def scann(ip):
+    # creat arp to request all hosts {**************************************************
+    #                                 ARP who has Net('192.168.217.1/24') says 192.168.217.154
+    #                                 **************************************************}
+
     RequestOf_ARP = scap.ARP(pdst=ip)
     print("*" * 50)
     print(RequestOf_ARP.summary())
     print("*" * 50)
-    #scap.ls(scap.ARP())
+    #scap.ls(scap.ARP()) #  this for lis options your intrested
 
 scann('192.168.217.1/24')
