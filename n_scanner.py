961c26d47e314a2dc17d1acb41077b88ad4c2b12
Hamad <33722717+hamad-SHE@users.noreply.github.com>
2020-10-05 10:33:23 +0300

    Update n_scanner.py

--- n_scanner.py
+++ n_scanner.py
@@ -2,17 +2,16 @@
 
 import scapy.all as scap
 
-def scann(ip):
-    # creat arp to request all hosts {**************************************************
-    #                                 ARP who has Net('192.168.217.1/24') says 192.168.217.154
-    #                                 **************************************************}
 
+def scann(ip):
+    #creat ARP request directry to brodcast MAC asking for IP
+  
     RequestOf_ARP = scap.ARP(pdst=ip)
     brodcast = scap.Ether(dst='ff:ff:ff:ff:ff:ff')
     RequestOf_ARP_brodcast = brodcast/RequestOf_ARP
     print("*" * 50)
     print(RequestOf_ARP_brodcast.summary())
     print("*" * 50)
-    #scap.ls(scap.ARP()) #  this for lis options your intrested
+    #scap.ls(scap.ARP()) #  this for list options your intrested
 
 scann('192.168.217.1/24')
